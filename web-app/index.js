bf989de11e5555f69a7d7f24c1ded89ddba25a7f
Emil Axelsson <nils.emil.axelsson@gmail.com>
2016-09-03 11:05:33 +0200

    add basic file structure and web server stub

---
+++ web-app/index.js
@@ -0,0 +1,8 @@
+let express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('HTTP Server started. Listening on 3000.');
+});
