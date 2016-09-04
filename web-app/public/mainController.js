f0e2a157d804fbb558e035eb3291acce0000e282
NaiLi <linnea.nabo@gmail.com>
2016-09-04 10:15:33 +0200

    data stufferi

--- web-app/public/mainController.js
+++ web-app/public/mainController.js
@@ -1,59 +1,30 @@
 let socket = new WebSocket('ws://localhost:3000/');
 
 angular.module('eltripleee', [])
-  .controller('mainController', ['$scope', function($scope) {
-
-  $scope.data; // data från armbandet
-  var pulseHistoryArray = [];
-
-  socket.onopen = function() {
-    console.log('Socket open.');
-  };
-
-  socket.onmessage = function(message) {
-    //console.log('Socket server message', message);
-    $scope.data = JSON.parse(message.data);
-    pulseHistoryArray.push($scope.data.pulse);
-    $scope.checkDeviation();
-  };
+  .controller('mainController', ['$scope', '$http', function($scope, $http) {
 
   $scope.title = "Postoperative department 1";
   $scope.subtitle = "University Hospital of Umeå";
+  $scope.pulseDeviation = false;
+  $scope.movementDeviation = false; 
 
-  $scope.roomData = {
-    rooms: [
-    {
-      id: 0,
-      roomNo: 1,
-      age: 65,
-      gender: "f",
-      medData: {
-        heartRate: 67,
-        oxygenSaturation: 500
-      },
-      movement: 5,
-      emergencyStatus: 10
-    },
-    {
-      id: 1,
-      roomNo: 2,
-      age: 75,
-      gender: "m",
-      medData: {
-        heartRate: 103,
-        oxygenSaturation: 500
-      },
-      movement: 2,
-      emergencyStatus: 20
-    }
-  ]};
+  $http({
+    method: 'GET',
+    url: '/data'
+  }).then(function successCallback(response) {
+    console.log(response)
+    $scope.roomData = response.data;
+  }, function errorCallback(response) {
+    console.log("error " + response.message);
+  });
+
 
   $scope.feedData = {
     feed: [
     {
       roomNo: 5,
       message: "Abnormal heart rate",
-      emergencyStatus: 10
+      emergencyStatus: 30
     }, 
     {
       roomNo: 17,
@@ -61,56 +32,59 @@ angular.module('eltripleee', [])
       emergencyStatus: 20
     }, 
     {
-      roomNo: 17,
+      roomNo: 18,
       message: "Long since last check",
-      emergencyStatus: 30
+      emergencyStatus: 10
     }
   ]};
 
+  socket.onopen = function() {
+    console.log('Socket open.');
+  };
+
+  socket.onmessage = function(message) {
+    //console.log('Socket server message', message);
+    var data = JSON.parse(message.data);
+    data.forEach((data) => {
+      $scope.roomData[data.id].data.push(data.data);
+      $scope.checkDeviation();
+    });
+  };
+
   $scope.selectedRoom;
- // $scope.selectedRoom = $scope.roomData.rooms[0]; //TODO ta bort
-  //$('#roominformationModal').modal('show'); //TODO ta bort
 
   $scope.individual = {
     id: 0, 
     pulse: 70, 
     time: 1472927439053, 
-    acceleration: 1.01, 
+    movement: 1.01, 
     location: [1, 2, 3]
   }
 
-/*
-  var source   = $("#roomlist").html();
-  var template = Handlebars.compile(source);
-  $('body').append(template(roomData));
-
-  var source   = $("#feedlist").html();
-  var template = Handlebars.compile(source);
-  $('body').append(template(feedData));
-
-
-  var source   = $("#roominformation").html();
-  var modalTemplate = Handlebars.compile(source);
-  //$('body').append(modalTemplate(roomData.rooms[0])); //Lägg rätt person
-*/
   $scope.onRoomClick = function(id) {
-        $scope.roomData.rooms[0].roomNo = 999;
-    console.log($scope.roomData.rooms[0].roomNo)
-    console.log("clicked on " + id);
-    $scope.selectedRoom = $scope.roomData.rooms[id];
-    //$('body').append(modalTemplate(roomData.rooms[id]));
+    $scope.selectedRoom = $scope.roomData[id];
     $('#roominformationModal').modal('show');
   }
 
   $scope.checkDeviation = function() {
-    if($scope.checkPulseDeviation()) {
-      $scope.createWarningToFeed("pulse", "High pulse", 10);
+    if(!$scope.movementDeviation && $scope.checkMovementDevation()) {
+      $scope.movementDeviation = true;
+      $scope.createWarningToFeed("pulse", "Moving outside of limit", 10);
     }
+    else if(!$scope.pulseDeviation && $scope.checkPulseDeviation()) {
+      $scope.pulseDeviation = true;
+      $scope.createWarningToFeed("pulse", "High pulse", 20);
+    }
+  }
+
+  $scope.checkMovementDevation = function() {
+    return true;
+    // IF lat > ....
   }
 
   $scope.checkPulseDeviation = function() {
     // Gör en grej!
-    if($scope.data.pulse > 3) {
+    if($scope.roomData[0].datapulse > 90) {
       return true;
     }
     return false;
@@ -122,7 +96,7 @@ angular.module('eltripleee', [])
       message: message,
       emergencyStatus: status
     })
-    $scope.feedData.feed[0].roomNo = 9;
+    $scope.$apply();
   }
 
 }]);
