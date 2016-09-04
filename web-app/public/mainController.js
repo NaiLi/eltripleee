1edf2ff5b8907261b5aaab31f3e5e289cdf90a41
NaiLi <linnea.nabo@gmail.com>
2016-09-04 08:56:34 +0200

    angular and popup

--- web-app/public/mainController.js
+++ web-app/public/mainController.js
@@ -1,8 +1,25 @@
-var eltripleee = angular.module('eltripleee', [])
+let socket = new WebSocket('ws://localhost:3000/');
+
+angular.module('eltripleee', [])
   .controller('mainController', ['$scope', function($scope) {
 
+  $scope.data; // data från armbandet
+  var pulseHistoryArray = [];
+
+  socket.onopen = function() {
+    console.log('Socket open.');
+  };
+
+  socket.onmessage = function(message) {
+    //console.log('Socket server message', message);
+    $scope.data = JSON.parse(message.data);
+    pulseHistoryArray.push($scope.data.pulse);
+    $scope.checkDeviation();
+  };
+
+
   $scope.title = "Avdelning XXX";
-  
+
   $scope.roomData = {
     rooms: [
     {
@@ -32,21 +49,35 @@ var eltripleee = angular.module('eltripleee', [])
   ]};
 
   $scope.feedData = {
-    feed: [ {
+    feed: [
+    {
       roomNo: 5,
       message: "Abnormal heart rate",
       emergencyStatus: 10
-    }, {
+    }, 
+    {
       roomNo: 17,
       message: "Low activity",
       emergencyStatus: 20
-    }, {
+    }, 
+    {
       roomNo: 17,
       message: "Long since last check",
       emergencyStatus: 30
-    }]
-  };
+    }
+  ]};
 
+  $scope.selectedRoom;
+ // $scope.selectedRoom = $scope.roomData.rooms[0]; //TODO ta bort
+  //$('#roominformationModal').modal('show'); //TODO ta bort
+
+  $scope.individual = {
+    id: 0, 
+    pulse: 70, 
+    time: 1472927439053, 
+    acceleration: 1.01, 
+    location: [1, 2, 3]
+  }
 /*
   var source   = $("#roomlist").html();
   var template = Handlebars.compile(source);
@@ -62,16 +93,36 @@ var eltripleee = angular.module('eltripleee', [])
   //$('body').append(modalTemplate(roomData.rooms[0])); //Lägg rätt person
 */
   $scope.onRoomClick = function(id) {
+        $scope.roomData.rooms[0].roomNo = 999;
+    console.log($scope.roomData.rooms[0].roomNo)
     console.log("clicked on " + id);
+    $scope.selectedRoom = $scope.roomData.rooms[id];
     //$('body').append(modalTemplate(roomData.rooms[id]));
-    //$('#roominformationModal').modal('show');
+    $('#roominformationModal').modal('show');
   }
 
-  $scope.closeModal = function() {
-    //console.log("closing modal")
-    //$('body').remove('#roominformation');
-
-    //$('#roominformationModal').modal('hide');
+  $scope.checkDeviation = function() {
+    if($scope.checkPulseDeviation()) {
+      $scope.createWarningToFeed("pulse", "High pulse", 10);
+    }
   }
+
+  $scope.checkPulseDeviation = function() {
+    // Gör en grej!
+    if($scope.data.pulse > 3) {
+      return true;
+    }
+    return false;
+  }
+
+  $scope.createWarningToFeed = function(type, message, status) {
+    $scope.feedData.feed.push({
+      roomNo: 201,
+      message: message,
+      emergencyStatus: status
+    })
+    $scope.feedData.feed[0].roomNo = 9;
+  }
+
 }]);
 
